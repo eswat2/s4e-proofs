4b6455ff1241d1ccfd3c3e7a9b04595d1223996b
Richard Hess <eswat2@gmail.com>
2020-11-23 17:12:02 -0800

    >> updating packages

--- rollup.config.js
+++ rollup.config.js
@@ -16,13 +16,7 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: css => {
-        css.write("public/build/bundle.css")
-      }
+      emitCss: false,
     }),
 
     // If you have external dependencies installed from
